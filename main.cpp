00f0a657550f18936ba36e73a76f8a116cc8aecd
Doctor He <sina.salahshour.32@gmail.com>
2021-05-19 04:27:46 +0000

    c007

--- main.cpp
+++ main.cpp
@@ -1,53 +1,14 @@
-#include <iostream> 
+#include <iostream>
+#include "Bignum.hpp" 
+
 #define OUT(x) cout << x << endl;
+
 using namespace std;
-string numadd(string num1, string num2)
-{
-	const string one = "1";
-	string * bignum;
-	string * smallnum;
-	string finalresult;
-	bool addition=false;
-	int currentsum=0;
-	if (num1.length() > num2.length()) {bignum = &num1; smallnum = &num2;} else {bignum = &num2; smallnum = &num1;}
-	char result[bignum->length()-1];
-	for(int i=0;i<smallnum->length();i++)
-	{
-		currentsum=(int)(bignum[0][bignum->length()-i-1]) + (int)(smallnum[0][smallnum->length()-i-1]) -48;
-		if (addition) 
-		{
-			addition = false;
-			currentsum += 1;
-		}
-		if (currentsum >= 58) {addition = true; currentsum -=10; }
-		result[bignum->length()-i-1] = (char)currentsum;
-	}
-	for(int i=0;i<(bignum->length()-smallnum->length());i++)
-	{
-		if(addition)
-		{
-			addition=false;
-			currentsum=(int)(bignum[0][bignum->length()-smallnum->length()-i-1])+1;
-			// OUT((char)currentsum)
-		if (currentsum >= 58) {addition = true; currentsum -=10; }
-			result[bignum->length()-smallnum->length()-i-1]=(char)currentsum;
-		}else
-		{
-			result[bignum->length()-smallnum->length()-i-1]=bignum[0][bignum->length()-smallnum->length()-i-1];
-		}
-	}
-	if(addition)
-		finalresult = one + result;
-	else
-		finalresult = result;
-	return finalresult;
-}
+
 int main()
 {
-	string big1;
-	string big2;
-	cin >> big1;
-	cin >> big2;
-	OUT(numadd(big1,big2))
+	Bignum bn1 = "1000000";
+	Bignum bn2 = "1";
+	OUT(bn1+bn2);
 	return 0;
 }
